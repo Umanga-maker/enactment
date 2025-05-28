62cca8dc095d37b1d85c762814d0914c418c3c93
Umanga-maker <limbuu063@gmail.com>
2025-05-28 18:04:31 +0545

    Delete day 21 directory

--- day 21/dayy 21/files.py
+++
@@ -1,7 +0,0 @@
-fo = open("./example.txt", "wb") 
-print("file contents:", fo)
-print("Closed or not:", fo.closed) 
-print("Opening mode: ", fo.mode) 
-fo.close() 
-print("Closed or not: ", fo.closed) 
-print("Opening mode: ", fo.mode)
